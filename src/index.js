01c318dd53dd5d0029b7690d532592bcacb2a2b6
Aleksey <alekseymishuk@gmail.com>
2018-09-21 23:10:10 +0300

    implement zerosCount method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  return getDivisionOnFive(number,1);
+}
+
+function getDivisionOnFive(number, power) {
+  const delimeter = Math.pow(5, power);
+  return number > delimeter ? 
+    Math.floor(number / delimeter) + getDivisionOnFive(number, ++power)
+    : Math.floor(number / delimeter);
 }
